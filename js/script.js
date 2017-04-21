d8364c2fb0c2efafb85d57af8987f9cdcc6316fe
Noemi <andreoli.noemi@gmail.com>
2017-04-21 16:39:48 +0100

    started refactoring js

--- js/script.js
+++ js/script.js
@@ -158,22 +158,29 @@ function updateSales(sale){
 
   createTotalSales(sale);
   createLiveSales(sale);
+  updateLatestSales(sale);
   addDataToLineChart(sale)
   myLineChart.update();
+  addRepeated(arrayGeneral, sale);
+  arrayGeneral.sort(toAsc);
   arrayGeneral.push(sale);
-  updateLatestSales(sale);
-  findAbsoluteBestSeller(sale);
-  if(arrayGeneral.length>0){
-    findBestSeller();
-  }
+  updateBestseller(arrayGeneral[0]);
+  // console.table(arrayGeneral);
   addDataToDon(sale);
   if (map){
     addPins(sale);
   }
-  if(sale.length>0){
-    lastReportTime= sales[sales.length-1].time;
-  }
+  lastReportTime= sale.time;
 };
+
+function addRepeated(arrayGeneral, sale){
+  for (obj of arrayGeneral){
+    if(obj.name == sale.name){
+      obj.sales += sale.sales;
+      console.log('hay copias!!');
+    }
+  }
+}
 
 function toAsc(a,b){
     return b.sales-a.sales;
@@ -183,98 +190,11 @@ function toDisc(a,b){
     return b.sales-a.sales;
 };
 
-function findBestSeller(){
-  $('#panel-body>div').removeClass('animate-in');
-    arrayGeneral.sort(toAsc);
-    //if there are sums
-    if(arraySums[0]){
-      //that are lower than the higher of the arrayGeneral, stick with the arrayGeneral
-        if(arrayGeneral[0].sales>arraySums[0].sales){
-          $('#panel-body>div').addClass('animate-in');
-            bestSellerH.innerHTML=arrayGeneral[0].sales;
-            bestSellerP.innerHTML=arrayGeneral[0].name;
-            getAddress(arrayGeneral[0].latitude,arrayGeneral[0].longitude);
-        }
-    //otherwise use the higher of the arrayGeneral
-    }else{
-      $('#panel-body>div').addClass('animate-in');
-        bestSellerH.innerHTML=arrayGeneral[0].sales;
-        bestSellerP.innerHTML=arrayGeneral[0].name;
-        getAddress(arrayGeneral[0].latitude, arrayGeneral[0].longitude);
-    }
-};
-
-function findAbsoluteBestSeller(sale){
-  //transform the arrayGeneral into an array of all names
-    arrayNames= sortArrayGByName();
-  //if (the last name is already present in the array of names) and (it's not the last one) it means it's a repetition
-    if(arrayNames.indexOf(sale.name)>0 && arrayNames.indexOf(sale.name) !== arrayNames.lastIndexOf(sale.name)){
-        let arrayNameRepeated= crateArrayRepeatedName(sale);
-        sum= findSum(arrayNameRepeated)
-
-        deleteCopies();
-        arraySums.push(sum);
-        arraySums.sort(toDisc);
-        // console.table(arraySums);
-        updateBestseller(arraySums[0]);
-    }
-}
-
-function deleteCopies(){
-  for (var obj of arraySums){
-    if (obj.name === sum.name){
-      removeByAttr(arraySums, 'name', obj.name);
-    }
-  }
-}
-
-function crateArrayRepeatedName(sale){
-  return arrayGeneral.filter(function(singleSale){
-    if(singleSale.name== sale.name){
-      return singleSale.name;
-    }
-  })
-};
-
-var removeByAttr = function(arr, attr, value){
-  var i = arr.length;
-  while(i--){
-    if(arr[i]
-      && arr[i].hasOwnProperty(attr)
-      && (arguments.length > 2 && arr[i][attr] === value)){
-        arr.splice(i,1);
-    }
-  }
-  return arr;
-}
-
-function sortArrayGByName(){
-  return arrayGeneral.map(function(singleSale){
-    return singleSale.name;
-  });
-}
-
-function findSum(arrayNameRepeated){
-  let sum= 0,
-    name,
-    lat,
-    long;
-  for (var item of arrayNameRepeated){
-    sum += item.sales;
-    name= item.name;
-    lat= item.latitude;
-    long= item.longitude;
-  }
-  return {sales :sum, name: name, latitude: lat, longitude: long};
-  }
-
 function updateBestseller(bestSeller){
-  //if there's sum higher than 9 (which is the highest the arrayGeneral can have)
-  if(bestSeller.sales>arrayGeneral[0].sales){
-    bestSellerH.innerHTML=bestSeller.sales;
-    bestSellerP.innerHTML=bestSeller.name;
-    getAddress(bestSeller.latitude,bestSeller.longitude);
-  }
+  $('#panel-body>div').addClass('animate-in');
+  bestSellerH.innerHTML=bestSeller.sales;
+  bestSellerP.innerHTML=bestSeller.name;
+  getAddress(bestSeller.latitude,bestSeller.longitude);
 }
 
 //create live sales table
@@ -385,62 +305,42 @@ function updateLatestSaleOne(sale){
                   "<br> Sales: " + sale.sales +
                   " at: " + prettyDate(new Date(sale.time));
   latestSaleOne.classList.add("animate-in");
-
   lsOne.appendChild(latestSaleOne);
 }
 
-var arrayLatestSales= [];
+var salesList= [];
 
 function updateLatestSaleTwo(sale){
   let lsTwo= document.getElementById("lsTwo");
 
-  arrayLatestSales.push(sale);
-  // console.log(arrayLatestSales);
-  if(arrayLatestSales[arrayLatestSales.length-2]){
+  salesList.push(sale);
+  if(salesList[salesList.length-2]){
     lsTwo.innerHTML= '';
-
     let latestSaleTwo= document.createElement("p");
 
-    latestSaleTwo.innerHTML=arrayLatestSales[arrayLatestSales.length-2].name +
-                    "<br> Sales: " + arrayLatestSales[arrayLatestSales.length-2].sales +
-                    " at: " + prettyDate(new Date(arrayLatestSales[arrayLatestSales.length-2].time));
+    latestSaleTwo.innerHTML=salesList[salesList.length-2].name +
+                    "<br> Sales: " + salesList[salesList.length-2].sales +
+                    " at: " + prettyDate(new Date(salesList[salesList.length-2].time));
     latestSaleTwo.classList.add("animate-in");
     setTimeout(function(){lsTwo.appendChild(latestSaleTwo)}, 150);
   }
 }
 
 function addDataToDon(sale){
-  let arrayOfBest= findArrayOfBest();
-  // console.table(arrayOfBest);
-  let arrayFor5Best= [];
-  let arrayOfFiveBest= findBestFive(arrayOfBest, arrayFor5Best);
-  dataDoughnut.labels= findArrayBest5Names(arrayOfFiveBest);
-  dataDoughnut.datasets[0].data= findArrayBest5Sales(arrayOfFiveBest);
+
+  var bestFive= arrayGeneral.slice(0,5);
+
+  bestFiveNames= bestFive.map(function(sale){
+    return sale.name;
+  })
+
+  bestFiveSales= bestFive.map(function(sale){
+    return sale.sales;
+  })
+
+  dataDoughnut.labels= bestFiveNames;
+  dataDoughnut.datasets[0].data= bestFiveSales;
   myDoughnutChart.update();
-}
-
-function findArrayOfBest(){
-  var arrayDeSingulos;
-  //add the array of sums to the arrayGeneral and then sort it ascending
-  if(arraySums.length>0){
-    for(sum of arraySums){
-      for(sale of arrayGeneral){
-        if(sum.name==sale.name){
-          arrayGeneral.filter(function(item, index, arr){
-            if(item.name == sum.name){
-              return arrayGeneral.splice(index, 1)
-            }
-          })
-        }
-      }
-    }
-
-    return arrayGeneral.concat(arraySums).sort(function(a,b){
-        return b.sales-a.sales;
-    });
-  }else{
-    return arrayGeneral;
-  }
 }
 
 function findBestFive(arrayOfBest, arrayFor5Best){
@@ -487,8 +387,6 @@ function createModalContent(divModal){
     if (event.target == divModal) {
       body.classList.remove('bg-noScroll');
       containerFluid.removeChild(containerFluid.children[1]);
-      // divModal.style.visibility= 'hidden';
-      // divModal.style.opacity= '0';
     }
   })
 }
@@ -507,10 +405,8 @@ function createModalSpan(divModal,divContent){
   divContent.appendChild(modalSpan);
   crateDivContainingP(divModal, divContent);
   modalSpan.addEventListener("click", function(){
-      containerFluid.removeChild(containerFluid.children[1]);
-      // divModal.style.visibility= 'hidden';
-      // divModal.style.opacity= '0';
-      body.classList.remove('bg-noScroll');
+    containerFluid.removeChild(containerFluid.children[1]);
+    body.classList.remove('bg-noScroll');
   })
 }
 
